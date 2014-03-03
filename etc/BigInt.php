a6fb31ec9000b3f24e43cba485eef95b5873a53e
Brad Broerman <bbroerman@bbroerman.net>
2014-03-03 18:11:00 -0500

    Update BigInt.php

--- etc/BigInt.php
+++ etc/BigInt.php
@@ -23,6 +23,20 @@
 // Brad Broerman
 // bbroerman@bbroerman.net
 //
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+//
 // IMPORTANT THING: Be sure to set maxDigits according to your precision
 // needs. Use the setMaxDigits() function to do this. See comments below.
 //
